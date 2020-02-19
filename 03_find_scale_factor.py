c1b708e04e1302b72f797d23d79e316edc2d97f2
FetanylAddiction <60630449+FetanylAddiction@users.noreply.github.com>
2020-02-20 09:09:25 +1300

    Caculates the Scale Factor
    Ask user for Desired Serving Amount and Serving Size

---
+++ 03_find_scale_factor.py
@@ -0,0 +1,20 @@
+# To Do
+
+# Ask user for servings made by recipe (and check this is a number more than 1/4)
+# Ask user for servings desired (check this is a number)
+# Calculate the scale factor
+# Warn the user if the sf is less than 0.25 or more than 4
+
+# Functions go here
+
+
+
+# Main Routine goes here
+
+serving_size = float(input("What is the recipe serving size?"))
+desired_size = float(input("How many servings are needed? "))
+
+scale_factor = desired_size / serving_size
+
+print("Scale Factor: {}".format(scale_factor))
+
