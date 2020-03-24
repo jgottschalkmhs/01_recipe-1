13be7f6343fc4d20054555f70d267c02569bde8c
FetanylAddiction <60630449+FetanylAddiction@users.noreply.github.com>
2020-03-24 13:13:54 +1300

    07_to_grams
    converts ingredients into grams

---
+++ 07_to_grams.py.py
@@ -0,0 +1,33 @@
+import csv
+
+# open file
+groceries = open("01_ingredients_ml_to_g.csv")
+
+# read data into a list
+csv_groceries = csv.reader(groceries)
+
+# create a dictionary to hold the data
+food_dictionary = {}
+
+# add the data from the list into the dictionary
+# (first item in row is key, next is definition)
+
+for row in csv_groceries:
+    food_dictionary[row[0]] = row[1]
+
+print(food_dictionary)
+
+keep_going = ""
+while keep_going == "":
+    amount = eval(input("How much? "))
+    amount = float(amount)
+
+    # get unit and change it to match dictionary
+    ingredient = input("Ingredient: ")
+
+    if ingredient in food_dictionary:
+        x_by = food_dictionary.get(ingredient)
+        amount = amount * float(x_by) / 250
+        print("Amount in g {}".format(amount))
+    else:
+        print("{} is unchanged".format(amount))
