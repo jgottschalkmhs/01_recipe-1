b54f9e060cde421c40d11493f4725550bb3f5096
FetanylAddiction <60630449+FetanylAddiction@users.noreply.github.com>
2020-03-24 13:15:47 +1300

    09_ingredient_splitter
    spilts ingredient list

---
+++ 09_ingredient_splitter.py
@@ -0,0 +1,54 @@
+import re
+
+# ingredient has mixed fraction followed by unit and ingredient
+
+full_recipe =[
+    "1 1/2 ml flour",
+    "3/4 cup milk",
+    "1 cup flour",
+    "2 tablespoons white sugar",
+    "1.5 tsp baking powder",
+    "pinch of cinnamon"
+]
+recipe_line = "1 1/2 ml flour"  # Change to input statement in due course
+
+mixed_regex = "\d{1,3}\s\d{1,3}\/\d{1,3}"
+for recipe_line in full_recipe:
+    recipe_line = recipe_line.strip()
+    # Get Amount...
+    if re.match(mixed_regex, recipe_line):
+
+        # Get mixed number by matching the regex
+        pre_mixed_num = re.match(mixed_regex, recipe_line)
+        mixed_num = pre_mixed_num.group()
+
+        # Replace space with a + sign
+        amount = mixed_num.replace(" ", "+")
+        # Change the string into a decimal
+        amount = eval(amount)
+
+        # Get unit and ingredient...
+        compile_regex = re.compile(mixed_regex)
+        unit_ingredient = re.split(compile_regex, recipe_line)
+        unit_ingredient = (unit_ingredient[1]).strip()  # remove extra white space from unit
+
+    else:
+        get_amount = recipe_line.split(" ", 1)  # spilt line at first space
+
+        try:
+            amount = eval(get_amount[0]) # convert amount to float if possible
+        except NameError:
+            amount = get_amount[0]
+            convert = "no"
+
+
+unit_ingredient = get_amount[1]
+
+get_unit = unit_ingredient.split(" ", 1)  # Splits text at first space
+print(get_unit)
+
+unit = get_unit[0]
+ingredient = get_unit[1]
+
+print(unit)
+print(ingredient)
